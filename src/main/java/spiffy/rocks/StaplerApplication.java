df9ddbe197e6138e46f3029bedc1c698712dd730
Andrew Hill <andy.hill.dev@gmail.com>
2017-09-09 15:41:53 +0100

    initial

---
+++ src/main/java/spiffy/rocks/StaplerApplication.java
@@ -0,0 +1,12 @@
+package spiffy.rocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaplerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaplerApplication.class, args);
+	}
+}
